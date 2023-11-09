36f70445a11df7844f42ca4a84da47ec4c7b706c
琉璃RURI\琉璃RURI <https://github.com/ENKAKOU/SEA-EDU.git>
2023-11-10 07:52:24 +0900

    2023.11.09-接口

---
+++ 2023.11.09-抽象/接口/Book.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+class Book {
+    private int no;
+    private String bookName;
+    private String author;
+
+
+    public Book(int no, String bookName, String author) {    // 构造
+        this.no = no;
+        this.bookName = bookName;
+        this.author = author;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
+
+
+interface BookOperate {                                // 图书 接口
+    Book addBook(int no, String bookName, String author);
+    void selectBook(Book[] books);
+}
+
+
+class BookOperateImpl implements BookOperate {         // 图书 实现类
+    @Override
+    public Book addBook(int no, String bookName, String author) {
+        if (no <= 0 || bookName.isEmpty() || author.isEmpty()) {
+            throw new IllegalArgumentException("输入参数不合法");
+        }
+        return new Book(no, bookName, author);
+    }
+
+    @Override
+    public void selectBook(Book[] books) {
+        for (Book book : books) {
+            System.out.println("《" + book.getBookName() + "》这本书的编号为" +
+                    book.getNo() + "，作者是" + book.getAuthor());
+        }
+    }
+}
+
+
+public class BookTest {                      // 图书测试类
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BookOperate bookOperate = new BookOperateImpl();
+
+        Book[] books = new Book[3];
+
+        for (int i = 0; i < 3; i++) {        // 添加图书信息
+            try {
+                System.out.print("请输入编号：");
+                int no = scanner.nextInt();
+                scanner.nextLine(); // 消费换行符
+                System.out.print("请输入图书名：");
+                String bookName = scanner.nextLine();
+                System.out.print("请输入作者：");
+                String author = scanner.nextLine();
+
+                books[i] = bookOperate.addBook(no, bookName, author);
+            } catch (IllegalArgumentException e) {
+                System.out.println("输入参数不合法：" + e.getMessage());
+                return;
+            }
+        }
+
+        bookOperate.selectBook(books);        // 打印图书信息
+    }
+}
---
+++ 2023.11.09-抽象/接口/User.java
@@ -0,0 +1,76 @@
+class User {
+    private int id;
+    private int password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+}
+
+
+interface UserCheck {                             // 用户操作接口
+    User addUser(int id, int password);
+    boolean checkUser(User user, int id, int password);
+}
+
+
+class UserCheckImpl implements UserCheck {        // 用户操作实现类
+    @Override
+    public User addUser(int id, int password) {
+        User user = new User();
+        user.setId(id);
+        user.setPassword(password);
+        System.out.println("添加成功");
+        return user;
+    }
+
+    @Override
+    public boolean checkUser(User user, int id, int password) {
+        if (user.getId() == id && user.getPassword() == password) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+import java.util.Scanner;
+
+public class UserTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        UserCheck userCheck = new UserCheckImpl();
+
+
+        System.out.print("请添加账户：");        // 添加账户
+        int id = scanner.nextInt();
+        System.out.print("请添加密码：");
+        int password = scanner.nextInt();
+        User user = userCheck.addUser(id, password);
+
+
+        System.out.print("请输入账户：");        // 登录验证
+        int inputId = scanner.nextInt();
+        System.out.print("请输入密码：");
+        int inputPassword = scanner.nextInt();
+
+        if (userCheck.checkUser(user, inputId, inputPassword)) {
+            System.out.println("登陆成功");
+        } else {
+            System.out.println("登录失败");
+        }
+    }
+}
